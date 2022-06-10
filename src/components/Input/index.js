dfb03a5cf148958dda06860ff59ba0d31355d973
quangoc98 <quanglm.classfunc@gmail.com>
2022-06-10 17:33:34 +0700

    fix component Input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,23 +1,37 @@
-import "./styles.css"
-import React from 'react'
+import "./styles.css";
+import React from "react";
 
 function Input(props) {
-    return (
-        <div>
-            <label for={props.name} className="form__label">{props.name}</label>
-            <input id={props.id} className="form__input" type={props.type} placeholder={props.placeholder}/>
-        </div>
-    )
+  return (
+    <div>
+      <label className="form__label">
+        {props.label__name}
+        <input
+          id={props.name}
+          name={props.name}
+          className="form__input"
+          type={props.type}
+          placeholder={props.placeholder}
+        />
+      </label>
+    </div>
+  );
 }
 
 function TextArea(props) {
-    return (
-        <div>
-            <label className="form__label">{props.name}</label>
-            <textarea className="form__input" placeholder={props.placeholder}></textarea>
-        </div>
-    )
+  return (
+    <div>
+      <label className="form__label">
+        {props.label__name}
+        <textarea
+          id={props.name}
+          name={props.name}
+          className="form__input"
+          placeholder={props.placeholder}
+        ></textarea>
+      </label>
+    </div>
+  );
 }
 
-
-export {Input, TextArea}
+export { Input, TextArea };
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -9,10 +9,10 @@ export default function Register() {
     <div className="wrapper__register">
         <div className="register">
             <Title title="Register"/>
-            <Input name="Username:" type="text" placeholder="Your username"/>
-            <Input name="Email:" type="email" placeholder="Your email"/>
-            <Input name="Phone" type="text" placeholder="Your phone number"/>
-            <Input name="Password:" type="password" placeholder="Your password"/>
+            <Input name__label="Username:" type="text" placeholder="Your username"/>
+            <Input name__label="Email:" type="email" placeholder="Your email"/>
+            <Input name__label="Phone" type="text" placeholder="Your phone number"/>
+            <Input name__label="Password:" type="password" placeholder="Your password"/>
 
             <Button text="Register"/>
         </div>
